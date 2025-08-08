6560e0edbcea47f4bbd53bb8ce7e70a9b7cd455d
Sembremos <sembremoseguridad@gmail.com>
2025-08-08 10:46:18 -0600

    Update app.py
    PNG

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+# --- Encabezado de imagen y texto justificado ---
+try:
+    st.image("logo_pavas.png", width=700)
+
 # --- Título y descripción ---
 st.title("Encuesta Comunidad Pavas 🏘️")
 st.markdown("Por favor, responda las siguientes preguntas sobre la seguridad en su barrio.")
